4146934b69de2344e26b68acb5b8c51d1906f750
Sven Günther <sven.guenther@it-agile.de>
2014-05-31 16:05:30 +0200

    Initial Commit

---
+++ QuintX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuintX
+//
+//  Created by Sven Günther on 31.05.14.
+//  Copyright (c) 2014 shg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuintX/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuintX
+//
+//  Created by Sven Günther on 31.05.14.
+//  Copyright (c) 2014 shg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
